9919a61bb94877af91ac969c226e00130d761c79
anicolauiesmanacor <anicolau@iesmanacor.cat>
2022-04-07 14:47:04 +0200

    back button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
     private GameObject sndManager;
+    private bool isGameCompleted = false;
 
     void Start()  {
         //SOUNDMANAGER
@@ -15,16 +17,26 @@ public class GameManager : MonoBehaviour {
     } 
 
     void initSoundManager() {
-        //sndManager.GetComponent<SoundManager>().musicVolume = PlayerPrefs.GetFloat("musicVolume");
         sndManager.GetComponent<SoundManager>().setMusicVolume(PlayerPrefs.GetFloat("musicVolume"));
-
         sndManager.GetComponent<SoundManager>().fxVolume = PlayerPrefs.GetFloat("fxVolume");
-        //sndManager.GetComponent<SoundManager>().setFXVolume(PlayerPrefs.GetFloat("fxVolume"));
-
-        //sndManager.GetComponent<SoundManager>().isMusicEnabled = PlayerPrefs.GetInt("music")==1?true:false;
         sndManager.GetComponent<SoundManager>().setEnableMusic(PlayerPrefs.GetInt("music")==1?true:false);
-
         sndManager.GetComponent<SoundManager>().isFXEnabled = PlayerPrefs.GetInt("fx")==1?true:false;
-        //sndManager.GetComponent<SoundManager>().setEnableFX(PlayerPrefs.GetInt("fx")==1?true:false);
+    }
+
+    void OnGUI() {
+        if (isGameCompleted) {
+            GUIStyle myButtonStyle = new GUIStyle(GUI.skin.button);
+            myButtonStyle.fontSize = 30;
+            if ( GUI.Button(new Rect(Screen.width/2-Screen.width/16, Screen.height/2-Screen.height/16, Screen.width/8, Screen.height/8), "REJUGAR", myButtonStyle)) {
+                restartGame();
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+
+    void restartGame() {
+        isGameCompleted = false;
+        //textWinner.gameObject.SetActive(false);
+        GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
     }
 }
